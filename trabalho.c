ca8bd2529b3854d6de4df156923972849a610e3b
matheusb-dev <leitinhogames6@gmail.com>
2024-10-20 19:53:37 -0300

    layout prontinho

--- trabalho.c
+++ trabalho.c
@@ -22,12 +22,82 @@ void gotoxy(int x, int y){
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 }
 
+//Funcão construção da tela Padrão
+void tela(){
+
+    int lin;
+    int col;
+
+    system("cls"); //limpar a tela
+    system("cls"); //limpa a tela
+
+    for (lin = 1; lin < 25; lin++)
+    {
+        gotoxy(01, lin);
+        printf("|");
+        gotoxy(79, lin);
+        printf("|");
+    }
+
+    for(col = 1; col < 80; col++){
+
+        gotoxy(col, 01);
+        printf("-");
+
+        gotoxy(col, 04);
+        printf("-");
+
+        gotoxy(col, 22);
+        printf("-");
+
+        gotoxy(col, 24);
+        printf("-");
+    }
+
+    gotoxy(01, 01);
+    printf("+");
+    gotoxy(79, 01);
+    printf("+");
+
+    gotoxy(01, 04);
+    printf("+");
+    gotoxy(79, 04);
+    printf("+");
+
+    gotoxy(01, 22);
+    printf("+");
+    gotoxy(79, 22);
+    printf("+");
+
+    gotoxy(01, 24);
+    printf("+");
+    gotoxy(79, 24);
+    printf("+");
+
+    gotoxy(02, 02);
+    printf("UNICV");
+
+    gotoxy(54, 02);
+    printf("ESTRURA DE DADOS - RODNEY");
+
+    gotoxy(51, 03);
+    printf("LISTA SIMPLESMENTE ENCADEADA");
+
+    gotoxy(02, 23);
+    printf("MSG: ");
+
+    gotoxy(7,02);
+    printf(" - Matheus B.D");
+
+    gotoxy(02,03);
+    printf("Data: 20/10/2024");
+}
 
 //Programa Principal
 void main(){
 
-    gotoxy(20,5);
-    printf("Hello World");
+    tela();
+    gotoxy(25,20);
 
 }
 
