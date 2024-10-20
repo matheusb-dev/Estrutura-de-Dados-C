12a295697bfe32cf5e7d01c817f9ced2e6e53322
matheusb-dev <leitinhogames6@gmail.com>
2024-10-20 19:41:14 -0300

    Primeiro boot

---
+++ trabalho.c
@@ -0,0 +1,23 @@
+/*  Nome: Matheus ezerra Domingos
+    RA: 166479-20224
+    Data: 20/10/2024
+    Objetivo:  manipulação de listas, incluindo 
+inserção, remoção, alteração e consulta de dados, além de persistência dessas 
+informações em disco. 
+
+*/
+
+//Importando as Bibliotecas
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <windows.h>
+
+//Programa Principal
+void main(){
+
+    printf("Hello World");
+    
+}
+
+
