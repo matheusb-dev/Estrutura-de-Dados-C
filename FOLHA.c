eb64fb0448b26279b26424d16ef859099d647a19
matheusb-dev <leitinhogames6@gmail.com>
2024-10-20 19:04:59 -0300

    Trabalho Rodney Início

---
+++ FOLHA.c
@@ -0,0 +1,16 @@
+/*  Nome: Matheus ezerra Domingos
+    RA: 166479-20224
+    Data: 20/10/2024
+    Objetivo: 
+ */
+
+//Importando as Bibliotecas
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <windows.h>
+
+//Programa Principal
+void main(){
+    printf("Hello World");
+}
